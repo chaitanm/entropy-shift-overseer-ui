0e7a2a42cc30d9c004c38314fd6e900ea1bb8df5
Chaitanya Meesala <chaitanya.m@endurance.com>
2017-03-13 15:43:51 +0530

    Configuring App Spec

---
+++ scripts/environment_setup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+sudo yum update
+
+sudo yum install ruby
+
+sudo yum install wget
+
+cd /home/ec2-user
+
+wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+
+chmod +x ./install
+
+sudo ./install auto
+
+sudo curl --silent --location https://rpm.nodesource.com/setup_7.x | sudo bash -
+
+yum -y install nodejs
